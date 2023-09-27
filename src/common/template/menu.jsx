57bbe36258fd1b9919bf4906e113d9954942770e
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-27 12:58:39 -0300

    first commit

---
+++ src/common/template/menu.jsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import MenuItem from "./menuItem";
+import MenuTree from "./menuTree";
+
+
+export default props =>(
+
+    <ul className="sidebar-menu">
+
+        <MenuItem path='#/' label='Dashboard' icon='dashboard' />
+
+
+        <MenuTree  label='cadastro' icon='edit' >
+        <MenuItem path='#billingCycles'/>
+
+
+        </MenuTree>
+         
+        
+        
+    </ul>
+)
---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./main/App";
+
+export default props => (
+    <div className="wrapper">
+        <h1>App</h1>
+
+        
+    </div>
+
+    
+)
+
+ReactDOM.render(<App />,document.getElementById('app'))
