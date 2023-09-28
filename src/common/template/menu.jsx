3ba4e16e3c672de0fdf6540e7b2ce014a4444208
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-28 08:46:06 -0300

    UPDATE: Added menuitem in menutree

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -4,20 +4,18 @@ import MenuItem from "./menuItem";
 import MenuTree from "./menuTree";
 
 
-export default props =>(
+export default props => (
 
     <ul className="sidebar-menu">
 
         <MenuItem path='#/' label='Dashboard' icon='dashboard' />
 
 
-        <MenuTree  label='cadastro' icon='edit' >
-        <MenuItem path='#billingCycles'/>
-
-
+        <MenuTree label='cadastro' icon='edit' >
+            <MenuItem path='#billingCycles' label='Ciclos de Pagamentos' icon='usd' />
         </MenuTree>
-         
-        
-        
+
+
+
     </ul>
 )
