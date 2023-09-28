085820a948286f3baa7eb43bb296977dbddb93eb
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-28 08:45:24 -0300

    UPDATE: Created footer file

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+        Copyright & Copy :2023
+        <a href="#" target="_blank">DEVemersonjr</a>
+    </strong>
+    
+  </footer>
+
+)
